d84b7186541339340311891552c7dc83b667c566
Dean Palm <dean@sxsw.com>
2018-10-15 17:53:14 -0500

    real fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+
+interactive_session = Session.create(
+  title: "Boost up with BlockChain Bots",
+  description: "Leverage bleeding edge technology with the public ledger.",
+  track: "Code and Development",
+  source: "https://BlockChain.io"
+)
+interactive_session.speakers.create(name: "Elon Musk")
+interactive_session.speakers.create(name: "Peter Thiel")
+interactive_session.speakers.create(name: "Mark Zuckerberg")
+interactive_session.speakers.create(name: "Mark Cuban")
+interactive_session.speakers.create(name: "Ajit Pai")
+interactive_session.speakers.create(name: "Jared Kushner")
+
+edu_session = Session.create(
+  title: "Privatize Education? Get real!",
+  description: "Are teachers over paid? You bet!",
+  track: "CE Learning",
+  source: "https://www.ed.gov/",
+)
+edu_session.speakers.create(name: "Betsy Devos")
+edu_session.speakers.create(name: "Ivanka Trump")
+edu_session.speakers.create(name: "Nikki Hailey")
+edu_session.speakers.create(name: "Maria Shriver")
