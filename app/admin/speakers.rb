5c1e7b84830f69b6c058cb1a7df78c430bcf7bf9
Dean Palm <dean@sxsw.com>
2018-10-15 17:46:57 -0500

    strong params

---
+++ app/admin/speakers.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Speaker do
+permit_params :name, :biography, :email, :speaker_agreement
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
